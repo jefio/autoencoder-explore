c36acf955dfc4d6096b770b7aaa06f6303d066e9
Jean-Francois Pessiot <jf.pessiot@gmail.com>
2017-09-12 22:58:03 +0900

    Add models.py

---
+++ models.py
@@ -0,0 +1,74 @@
+"""
+Models
+"""
+from keras.layers import Input, Dense, Lambda, Layer
+from keras.models import Model
+from keras import backend as K
+from keras import metrics
+
+
+def get_vae_models(sizes):
+    """
+    Based on the Keras VAE example script.
+    """
+    #
+    epsilon_std = 1.0
+    original_dim, intermediate_dim, latent_dim = sizes
+
+    x = Input(shape=(original_dim,))
+    h = Dense(intermediate_dim, activation='relu')(x)
+    z_mean = Dense(latent_dim)(h)
+    z_log_var = Dense(latent_dim)(h)
+
+    def sampling(args):
+        z_mean, z_log_var = args
+        epsilon = K.random_normal(shape=(K.shape(z_mean)[0], latent_dim), mean=0.,
+                                  stddev=epsilon_std)
+        return z_mean + K.exp(z_log_var / 2) * epsilon
+
+    # note that "output_shape" isn't necessary with the TensorFlow backend
+    z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
+
+    # we instantiate these layers separately so as to reuse them later
+    decoder_h = Dense(intermediate_dim, activation='relu')
+    decoder_mean = Dense(original_dim, activation='sigmoid')
+    h_decoded = decoder_h(z)
+    x_decoded_mean = decoder_mean(h_decoded)
+
+
+    class CustomVariationalLayer(Layer):
+        def __init__(self, **kwargs):
+            self.is_placeholder = True
+            super(CustomVariationalLayer, self).__init__(**kwargs)
+
+        def vae_loss(self, x, x_decoded_mean):
+            xent_loss = original_dim * metrics.binary_crossentropy(x, x_decoded_mean)
+            kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
+            return K.mean(xent_loss + kl_loss)
+
+        def call(self, inputs):
+            x = inputs[0]
+            x_decoded_mean = inputs[1]
+            loss = self.vae_loss(x, x_decoded_mean)
+            self.add_loss(loss, inputs=inputs)
+            # We won't actually use the output.
+            return x
+
+    y = CustomVariationalLayer()([x, x_decoded_mean])
+    vae = Model(x, y)
+    vae.compile(optimizer='rmsprop', loss=None)
+
+    # build a model to project inputs on the latent space
+    encoder = Model(x, z_mean)
+
+    # build a digit generator that can sample from the learned distribution
+    decoder_input = Input(shape=(latent_dim,))
+    _h_decoded = decoder_h(decoder_input)
+    _x_decoded_mean = decoder_mean(_h_decoded)
+    decoder = Model(decoder_input, _x_decoded_mean)
+
+    return {
+        'autoencoder': vae,
+        'encoder': encoder,
+        'decoder': decoder
+    }
